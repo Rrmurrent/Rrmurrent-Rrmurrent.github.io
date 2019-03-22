0b6c3080b8cf7440f976033618284324fcc2f7a3
Smarty Pants <amcurrent@hotmail.com>
2019-03-22 08:01:42 -0500

    added index,style,logic

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+
+
+function viewProjects() {
+     console.log('the buttons work');
+ };
+
+$(document).ready(() => {
+
+
+ 
+viewProjects();
+
+// $('.btn aqua-gradient').addClass('animated zoomOutRight');
+
+});
+ 
