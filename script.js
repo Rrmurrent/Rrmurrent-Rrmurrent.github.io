e3d340ec96332d301c0df314bf398d64622469b8
Smarty Pants <amcurrent@hotmail.com>
2020-02-04 19:51:08 -0600

    added jquery for navbar functionality

--- script.js
+++ script.js
@@ -1,17 +1,93 @@
 
-   
-const nav = document.querySelector('#main');
-let topOfNav = nav.offsetTop;
+$(document).ready(() =>{
 
-function fixNav() {
-  if (window.scrollY >= topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
-  } else {
-    document.body.classList.remove('fixed-nav');
-    document.body.style.paddingTop = 0;
+  $('.homeDiv').show();
+  $('.aboutDiv').hide();
+  $('.projectsDiv').hide();
+  $('.contactDiv').hide();
+  $('.resumeDiv').hide();
+  
+
+  const nav = document.querySelector('#main');
+  let topOfNav = nav.offsetTop;
+  
+  function fixNav() {
+    if (window.scrollY >= topOfNav) {
+      document.body.style.paddingTop = nav.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.classList.remove('fixed-nav');
+      document.body.style.paddingTop = 0;
+    }
   }
-}
+  
+  window.addEventListener('scroll', fixNav);
+  
+  // Navbar functionality
 
-window.addEventListener('scroll', fixNav);
+  $('.nav-item-about').on("click", function(){
 
+      $('.aboutDiv').show();
+      $('.homeDiv').hide();
+      $('.projectsDiv').hide();
+      $('.contactDiv').hide();
+      $('.resumeDiv').hide();
+      
+      
+     
+      console.log('about nav works');
+  });
+
+
+
+  $('.nav-item-projects').on("click", function(){
+
+    $('.projectsDiv').show();
+    $('.homeDiv').hide();
+    $('.aboutDiv').hide();
+    $('.contactDiv').hide();
+    $('.resumeDiv').hide();
+    
+   
+    console.log('project nav works');
+});
+
+$('.nav-item-contact').on("click", function(){
+
+  $('.contactDiv').show()
+  $('.homeDiv').hide();
+  $('.aboutDiv').hide();
+  $('.resumeDiv').hide();
+  $('.projectsDiv').hide();
+  ;
+ 
+  console.log('contact nav works');
+});
+
+$('.nav-item-resume').on("click", function(){
+
+  $('.resumeDiv').show();
+  $('.homeDiv').hide();
+  $('.aboutDiv').hide();
+  $('.projectsDiv').hide();
+  $('.contactDiv').hide();
+  
+ 
+  console.log('resume nav works');
+});
+
+
+$('.logo').on("click", function(){
+
+  $('.homeDiv').show();
+  $('.aboutDiv').hide();
+  $('.projectsDiv').hide();
+  $('.contactDiv').hide();
+  $('.resumeDiv').hide();
+ 
+  console.log('logo home nav works');
+});
+// End Navbar functionality
+
+// End document.ready function
+});
